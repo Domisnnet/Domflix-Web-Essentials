6c865574c12ee3512da5dfd3d1f6540e80b297ba
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-04-14 01:12:29 +0000

    OK

--- src/js/data.js
+++ src/js/data.js
@@ -1,124 +1,41 @@
 export const categories = [
-  {
-    title: 'Épicos de guerra',
-    items: [
-      {
-        img: 'https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/1917-1.jpg',
-        top10: true,
-        badge: 'Clássico',
-        badgeColor: 'red',
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=YqNYrYUiMfg',
-      },
-      {
-        img: 'https://i.pinimg.com/originals/a4/76/34/a476342f2a7b88f6c49a6288883675f6.jpg',
-        progress: 20,
-        youtube: 'https://www.youtube.com/watch?v=F-eMt3SrfFU',
-      },
-      {
-        img: 'https://br.web.img2.acsta.net/pictures/14/10/21/19/46/406282.jpg',
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=wz1c9c43s-E',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/8/87/A_Batalha_do_Passo_Esquecido.jpg',
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=42-s_m-h_uE',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/e/e6/Dunkirk_Poster.jpg',
-        top10: true,
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=F-eMt3SrfFU',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/thumb/a/a2/O_Resgate_do_Soldado_Ryan.jpg/250px-O_Resgate_do_Soldado_Ryan.jpg',
-        top10: true,
-        badge: 'Clássico',
-        badgeColor: 'red',
-        progress: 90,
-        youtube: 'https://www.youtube.com/watch?v=RYNh-v0rrqA',
-      },
-    ],
-  },
-  {
-    title: 'Séries que vão te prender',
-    items: [
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/9/92/La_casa_de_papel.png',
-        top10: true,
-        badge: 'Nova temporada',
-        badgeColor: 'red',
-        progress: 40,
-        youtube: 'https://www.youtube.com/watch?v=3y-6o_j2dXY',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/thumb/d/d4/The_Witcher_2019.jpg/250px-The_Witcher_2019.jpg',
-        top10: true,
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=tjujvMkqWe4',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/b/b9/Stranger_things_temporada_4.jpg',
-        badge: 'Novo episódio',
-        badgeColor: 'red',
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=b1He8_Y0eWI',
-      },
-      {
-        img: 'https://i.pinimg.com/originals/c9/a2/4c/c9a24c7f3f3172445e99a8f4c284777d.jpg',
-        badge: 'Novidade',
-        badgeColor: 'red',
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=d_3-S_6Kq3c',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/f/f3/The_Mandalorian.jpg',
-        top10: true,
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=aWzlQ2N66mw',
-      },
-    ],
-  },
-  {
-    title: 'Para maratonar',
-    items: [
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/7/7a/The_crown_cartaz_promocional.jpg',
-        top10: true,
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=5hAXVq-563c',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/thumb/e/e4/Peaky_Blinders_T5.jpg/250px-Peaky_Blinders_T5.jpg',
-        top10: true,
-        badge: 'Novidade',
-        badgeColor: 'red',
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=oVzVdvGVTCI',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/thumb/8/8a/Dark_poster.jpg/250px-Dark_poster.jpg',
-        top10: true,
-        badge: 'Novo episódio',
-        badgeColor: 'red',
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=JkYAnTrsXfE',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/thumb/b/b5/Ozark_season_3_poster.jpg/250px-Ozark_season_3_poster.jpg',
-        top10: true,
-        badge: 'Novo episódio',
-        badgeColor: 'red',
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=oAyY0l3-GiI',
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/pt/thumb/e/ea/The_Queen%27s_Gambito.jpg/250px-The_Queen%27s_Gambito.jpg',
-        top10: true,
-        progress: 0,
-        youtube: 'https://www.youtube.com/watch?v=oAyY0l3-GiI',
-      },
-    ],
-  },
+    {
+        title: "Épicos",
+        items: [
+            {
+                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+            },
+            {
+                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    }
 ];
